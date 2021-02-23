af71ece3338930549f417ffefddf36763390e1ab
a.ashmarina <a.ashmarina@alef.im>
2021-02-23 22:25:23 +0300

    во view добавлен вывод списка яблок

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -15,6 +15,26 @@ $this->title = 'My Yii Application';
     </div>
 
     <div class="body-content">
-
+        <?php if(!empty($arr_apples)):
+        $model = $arr_apples;
+        foreach ($arr_apples as $apple): ?>
+        <?php $apple->hours_after_fallout > Yii::$app->params['hours_freshness_apple'] ? $status = 'гнилое' : $status = $apple->status ?>
+        <div class = "panel panel-default">
+            <div class = "panel-heading" >
+                <h4 class = "panel-heading" style="line-height: 1.5">
+                    яблоко номер: <?= $apple->id ?><br>
+                    цвет: <?= $apple->color ?><br>
+                    статус: <?= $status ?><br>
+                    <?php if(!empty($apple->size)): ?>
+                        размер: <?= $apple->size ?><br>
+                    <?php endif; ?>
+                    дата появления: <?= $apple->appearance_date ?><br>
+                    <?php if(!empty($apple->fallout_date)): ?>
+                        дата падения: <?= $apple->fallout_date ?>
+                    <?php endif; ?>
+                </h4>
+            </div>
+        </div>
+        <?php endforeach; endif;?>
     </div>
 </div>
